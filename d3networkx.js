7bae187b8205e4c75c704af5ba04eba3715481eb
unknown <mattik15@gmail.com>
2019-11-24 21:05:24 +0100

    Drawing test

--- d3networkx.js
+++ d3networkx.js
@@ -4,34 +4,20 @@ window.onload = function init()
     var jsnx = require('jsnetworkx');
 
     var G = new jsnx.Graph();
- 
-    G.addNodesFrom([1,2,3,4], {group:0});
-    G.addNodesFrom([5,6,7], {group:1});
-    G.addNodesFrom([8,9,10,11], {group:2});
-     
-    G.addPath([1,2,5,6,7,8,11]);
-    G.addEdgesFrom([[1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]]);
-     
-    var color = d3.scale.category20();
+    G.addNode(1, {count: 12});
+    G.addNode(2, {count: 8});
+    G.addNode(3, {count: 15});
+    G.addEdgesFrom([[1,2],[2,3]]);
+    console.log(G.nodes())
     jsnx.draw(G, {
-        element: '#canvas',
-        layoutAttr: {
-            charge: -120,
-            linkDistance: 20
-        },
-        nodeAttr: {
-            r: 5,
-            title: function(d) { return d.label;}
-        },
-        nodeStyle: {
-            fill: function(d) { 
-                return color(d.data.group); 
-            },
-            stroke: 'none'
-        },
-        edgeStyle: {
-            fill: '#999'
+      element: '#demo-canvas',
+      withLabels: true,
+      nodeAttr: {
+        r: function(d) {
+          // `d` has the properties `node`, `data` and `G`
+          return d.data.count;
         }
+      }
     });
 
     console.log(G.nodes())
